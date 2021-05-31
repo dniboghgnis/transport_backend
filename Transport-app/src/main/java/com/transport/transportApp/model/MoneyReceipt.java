166ed3fa21db44fca7fa04d4f61f9ea1e4f85dc4
anjon <anjon@Bernabeu>
2021-05-31 23:49:16 +0530

    Updated moneyReceipt class

--- Transport-app/src/main/java/com/transport/transportApp/model/MoneyReceipt.java
+++ Transport-app/src/main/java/com/transport/transportApp/model/MoneyReceipt.java
@@ -1,10 +1,33 @@
 package com.transport.transportApp.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tbl_moneyReceipt")
 public class MoneyReceipt {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Mr_Id", unique = true, nullable = false)
+	private long mrId;
+
+	public long getMrId() {
+		return mrId;
+	}
+
+	public void setMrId(long mrId) {
+		this.mrId = mrId;
+	}
+
+	@Override
+	public String toString() {
+		return "MoneyReceipt [mrId=" + mrId + "]";
+	}
+	
+	
 }
