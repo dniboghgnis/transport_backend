a32a67dcc0f70f07db9147091879bc5fabc2615d
anjon <anjon@Bernabeu>
2021-05-31 22:44:17 +0530

    Updated Manifest entity class

--- Transport-app/src/main/java/com/transport/transportApp/model/Consignment.java
+++ Transport-app/src/main/java/com/transport/transportApp/model/Consignment.java
@@ -1,10 +1,18 @@
 package com.transport.transportApp.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tbl_consigner")
 public class Consignment {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Consignment_Id", unique = true)
+	private int id;
 }
--- Transport-app/src/main/java/com/transport/transportApp/model/Manifest.java
+++ Transport-app/src/main/java/com/transport/transportApp/model/Manifest.java
@@ -43,11 +43,103 @@ public class Manifest {
 	@Column(name = "Eway_Bill")
 	private String ewayBill;
 	
+	@Column(name = "Last_Serial_Number")
+	private int lastSerialNumber;
+	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "Truck_Id")
 	private Truck ownerSName;
 	
 	@Column(name = "Consignment_Id")
 	private List<String> consignmentId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getManifestNumber() {
+		return manifestNumber;
+	}
+
+	public void setManifestNumber(int manifestNumber) {
+		this.manifestNumber = manifestNumber;
+	}
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public Truck getTruckRef() {
+		return truckRef;
+	}
+
+	public void setTruckRef(Truck truckRef) {
+		this.truckRef = truckRef;
+	}
+
+	public Branch getFrom() {
+		return from;
+	}
+
+	public void setFrom(Branch from) {
+		this.from = from;
+	}
+
+	public Branch getTo() {
+		return to;
+	}
+
+	public void setTo(Branch to) {
+		this.to = to;
+	}
+
+	public String getEwayBill() {
+		return ewayBill;
+	}
+
+	public void setEwayBill(String ewayBill) {
+		this.ewayBill = ewayBill;
+	}
+
+	public int getLastSerialNumber() {
+		return lastSerialNumber;
+	}
+
+	public void setLastSerialNumber(int lastSerialNumber) {
+		this.lastSerialNumber = lastSerialNumber;
+	}
+
+	public Truck getOwnerSName() {
+		return ownerSName;
+	}
+
+	public void setOwnerSName(Truck ownerSName) {
+		this.ownerSName = ownerSName;
+	}
+
+	public List<String> getConsignmentId() {
+		return consignmentId;
+	}
+
+	public void setConsignmentId(List<String> consignmentId) {
+		this.consignmentId = consignmentId;
+	}
+
+	@Override
+	public String toString() {
+		return "Manifest [id=" + id + ", manifestNumber=" + manifestNumber + ", dateTime=" + dateTime + ", truckRef="
+				+ truckRef + ", from=" + from + ", to=" + to + ", ewayBill=" + ewayBill + ", lastSerialNumber="
+				+ lastSerialNumber + ", ownerSName=" + ownerSName + ", consignmentId=" + consignmentId + "]";
+	}
+	
+	
 
 }
