4f74fdd87779ba32194483a4571e1755a4c8bac4
anjon <anjon@Bernabeu>
2021-06-26 00:16:29 +0530

    Updated with latest changes

--- Transport-app/src/main/java/com/transport/transportApp/TransportAppApplication.java
+++ Transport-app/src/main/java/com/transport/transportApp/TransportAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransportAppApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransportAppApplication.class, args);
